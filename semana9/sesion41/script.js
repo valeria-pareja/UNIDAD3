7ea075f8fe409628db13e889ed6f8bda7785034a
valeria-pareja <valparejasoto@crackthecode.la>
2024-08-28 20:55:03 -0500

    g

---
+++ semana9/sesion41/script.js
@@ -0,0 +1,45 @@
+/*let number = [1,2,3,4];
+number.push(5); //devuelve la longitud
+console.log(number);*/
+
+//método pop
+/*let number2 = [2,4,6,8,3,7,6];
+number2.pop(); //no necesita parametro sale todomenos el 6
+console.log(number2);*/
+
+/*let number2 = [2,4,6,8,3,7,6];
+console.log(number2.pop()); //
+console.log(number2);*/
+
+/*let number3 = ['b','c','s']
+number3.unshift('z');
+console.log(number3);
+number3.shift();
+console.log(number3);*/
+
+//let numeros = [2,5,7];
+//numeros.push (4);
+//numeros.pop(); //elimina el ultimo 4
+//numeros.shift(); //elimina el inicio
+//numeros.unshift(1); //añade al inicio
+//console.log(numeros); // 1,5,7
+
+//let arraynuevo = numeros.slice(0,2);
+//console.log(arraynuevo);
+//console.log(numeros);
+
+//let edades=[11,20,8,13,5,10];
+//let rango = edades.slice(2,4);
+//console.log(rango);
+//console.log(redades);
+
+//let edades=[11,20,8,13,5,10];
+//edades.splice(4,2,15,20);
+//console.log(edades);
+
+let array1=[2,4,6,8];
+let array2=[3,5,7,9];
+let array3=[5,10,15,20];
+
+let array_concat=array1.concat(array2,array3);
+console.log(array_concat)
